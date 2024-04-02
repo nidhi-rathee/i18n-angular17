dd746591916c51428d386e63357f536bcbcea943
Nidhi Rathee <nidhi.rathee@oneadvanced.com>
2024-04-02 17:16:31 +0530

    working with ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { locales } from './locale';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,7 +15,12 @@ export class AppComponent {
   selectedLocale = 'en';
   title = 'i18n';
 
+  constructor(private translateService: TranslateService) {}
+
   onChangeLocale(event: any) {
+    console.log('event', event.target.value)
     this.selectedLocale = event.target.value;
+    this.translateService.setDefaultLang(event.target.value)
+    this.translateService.use(event.target.value)
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),
+    importProvidersFrom(HttpClientModule),
+  importProvidersFrom(TranslateModule.forRoot({
+    loader: {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [HttpClient]
+    }
+  }))]
 };
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/locale/', '.json')
+}
